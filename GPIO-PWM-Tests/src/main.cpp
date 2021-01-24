74531fd5ef767484a36b1476249905f91c3960e6
Chrismettal <chrismettal@web.de>
2021-01-24 16:57:54 +0100

    Testing pins 12-16 for PWM
    - GPIO16 is HIGH during boot

--- GPIO-PWM-Tests/src/main.cpp
+++ GPIO-PWM-Tests/src/main.cpp
@@ -1,9 +1,39 @@
+//https://github.com/Chrismettal/ESP-Hiro
+
+// testing program for evaluating GPIOs on ESP8266 for valid inputs/outputs
+
 #include <Arduino.h>
 
+int pwmpins[5] = {12,13,14,15,16};
+
+bool direction;
+
+/**************************************SETUP********************************************/
 void setup() {
-  // put your setup code here, to run once:
+
+  for (int i = 0; i < 6; i++) {
+    pinMode(pwmpins[i], OUTPUT);
+  }
+  
 }
 
+/*************************************LOOP**********************************************/
 void loop() {
-  // put your main code here, to run repeatedly:
+  
+  for (int i = 0; i <= 5; i++) {
+    if (!direction) {
+      for (int j = 0; j <= 1024; j++) {
+        analogWrite(pwmpins[i],j);
+        delayMicroseconds(250);
+      }
+    } else {
+      for (int j = 1024; j >= 0; j--) {
+        analogWrite(pwmpins[i],j);
+        delayMicroseconds(250);
+      }
+    }
+  }
+
+  direction = !direction;
+
 }
